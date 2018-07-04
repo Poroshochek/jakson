1652057b37262510322003994b873d277f8c9a77
Poroshochek <mr.soluk@gmail.com>
2018-07-04 17:40:27 +0300

    prepare our models | create links objects

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    public function post()
+    {
+        return $this->hasOne(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->hasOne(User::class);
+    }
 }

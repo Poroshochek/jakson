184101c4e84bb731278fd378aaa48b52171e501d
Poroshochek <mr.soluk@gmail.com>
2018-07-05 17:46:27 +0300

    finish model Posts end prepare User

--- app/Post.php
+++ app/Post.php
@@ -2,12 +2,18 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Model;
 use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
     use Sluggable;
+
+    const IS_DRAFT = 0;
+    const IS_PUBLIC = 1;
+
+    protected $fillable = ['title', 'content'];
 
     public function category()
     {
@@ -37,4 +43,104 @@ class Post extends Model
             ]
         ];
     }
+
+    public static function add($filds)
+    {
+        $post =  new self;
+        $post->fill($filds);
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($filds)
+    {
+        $this->fill($filds);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        Storage::delete('uploads/' . $this->image);
+        $this->delete();
+    }
+
+    public function uploadImage($image)
+    {
+        if($image == null) { return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getImage()
+    {
+        if($this->image == null) {
+            return '/img/no-image.png';
+        }
+
+        return '/uploads/' . $this->image;
+    }
+
+    public function setCategory($id)
+    {
+        if($id == null) { return; }
+
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function setTegs($ids)
+    {
+        if($ids == null) { return; }
+
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+        $this->status = Post::IS_DRAFT;
+        $this->save();
+    }
+
+    public function setPublic()
+    {
+        $this->status = Post::IS_PUBLIC;
+        $this->save();
+    }
+
+    public function toggleStatus($val)
+    {
+        if ($val == null) {
+            return $this->setDraft();
+        }
+
+        return $this->setPublic();
+    }
+
+    public function setFeatured()
+    {
+        $this->is_featured = 0;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->is_featured = 1;
+        $this->save();
+    }
+
+    public function toggleFeatured($val)
+    {
+        if ($val == null) {
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
+    }
+
 }
--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -35,5 +36,49 @@ class User extends Authenticatable
     public function comments()
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public static function add($fields)
+    {
+        $user = new self;
+        $user->fill($fields);
+        $user->password = bcrypt($fields['password']);
+        $user->save();
+
+        return $user;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->password = bcrypt($fields['password']);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        Storage::delete('uploads/' . $this->image);
+        $this->remove();
+    }
+
+    public function uploadAvatar($image)
+    {
+        if($image == null) { return; }
+
+        Storage::delete('uploads/' . $this->image);
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->saveAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function getAvatar()
+    {
+        if($this->image = null)
+        {
+            return '/img/no-avatar-img.png';
+        }
+
+        return '/uploads/' . $this->image;
     }
 }
